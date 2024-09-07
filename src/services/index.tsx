1b3eda9c947e93d59e13c24e15156b3a6f91defa
Arthur Pereira da Silva <arthurnpdasilva@gmail.com>
2024-09-07 18:09:39 -0300

    wlp

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { enqueueSnackbar } from 'notistack';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_URL,
+  baseURL: 'http://localhost:8080',
 });
 
 export const config = {
