1409e30d21427899371bcc84faab3419aec0daac
Wesley_ <47366440+wesleysousaa@users.noreply.github.com>
2024-12-13 22:01:32 -0300

    fix: error messages for back

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import { clearToken, getToken } from '@/utils/tokens';
 import axios from 'axios';
+import { enqueueSnackbar } from 'notistack';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_URL,
@@ -13,6 +14,7 @@ export const config = {
 
 const HTTP_STATUS = {
   UNAUTHORIZED: 401,
+  INTERNAL_SERVICE_ERROR: 500,
 };
 
 api.interceptors.response.use(
@@ -24,6 +26,26 @@ api.interceptors.response.use(
       clearToken();
       window.location.href = '/';
     }
+
+    let errorMessage = 'Ocorreu um erro desconhecido.';
+
+    if (error.response) {
+      if (error.response.data) {
+        if (error.response.data.message) {
+          errorMessage = error.response.data.message;
+        } else if (Array.isArray(error.response.data.messages)) {
+          errorMessage = error.response.data.messages.join('\n');
+        } else if (typeof error.response.data === 'string') {
+          errorMessage = error.response.data;
+        }
+      } else if (error.response.status === HTTP_STATUS.INTERNAL_SERVICE_ERROR) {
+        errorMessage = 'Erro interno no servidor.';
+      }
+    } else if (error.message) {
+      errorMessage = error.message;
+    }
+
+    enqueueSnackbar(errorMessage, { variant: 'error' });
 
     return Promise.reject(error);
   },
